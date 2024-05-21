ab6ac4cbb96086d883bd4dda3be250d5def9c20b
Irene Goff <ireneegoff@gmail.com>
2024-05-21 17:49:20 -0400

    Test accelerate and brake methods on Car instances.

--- cc3.js
+++ cc3.js
@@ -15,3 +15,19 @@ brake() {
     console.log('New speed of ' + this.make + ': ' + this.speed + ' km/h');
 }
 }
+// Car 1: BMW starting at 120 km/h
+let car1 = new Car('BMW', 120);
+car1.accelerate();
+car1.accelerate();
+car1.brake();
+car1.brake();
+car1.brake();
+
+// Car 2: Mercedes starting at 95 km/h
+let car2 = new Car('Mercedes', 95);
+car2.accelerate();
+car2.brake();
+car2.brake();
+car2.accelerate();
+car2.accelerate();
+car2.brake();
